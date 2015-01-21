0b9ef2549029349301d5e4354cea9984088b2f82
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-21 11:06:01 -0800

    avl tree exercise complete

--- avl.js
+++ avl.js
@@ -27,20 +27,160 @@ AVLTree.prototype.balance = function() {
     }
     this.rotateLL();
   } else if (ldepth + 1 < rdepth) {
-        // RR or RL rorarion
-        var rrdepth = this.right.right == null ? 0 : this.right.right.depth;
-        var rldepth = this.right.left  == null ? 0 : this.right.left.depth;
- 
-        if (rldepth > rrdepth) {
-            // RR rotation consists of a LL rotation of the right child
-            this.right.rotateLL();
-            // plus a RR rotation of this node, which happens anyway
-        }
-        this.rotateRR();
+    // RR or RL rorarion
+    var rrdepth = this.right.right == null ? 0 : this.right.right.depth;
+    var rldepth = this.right.left  == null ? 0 : this.right.left.depth;
+
+    if (rldepth > rrdepth) {
+        // RR rotation consists of a LL rotation of the right child
+        this.right.rotateLL();
+        // plus a RR rotation of this node, which happens anyway
+    }
+    this.rotateRR();
   }
 };
 
 AVLTree.prototype.rotateLL = function() {
   // the left side is too long => rotate from the left (_not_ leftwards)
-  this.elementsBefore = this.elements
+  this.elementsBefore = this.elements;
+  this.rightBefore = this.right;
+  this.nodeBefore = this.node;
+  this.elements = this.left.elements;
+  this.right = this.left;
+  this.left = this.left.left;
+  this.right.left = this.right.right;
+  this.right.right = rightBefore;
+  this.right.node = nodeBefore;
+  this.right.elements = elementsBefore;
+  this.right.updateInNewLocation();
+  this.updateInNewLocation();
 };
+
+AVLTree.prototype.rotateRR = function() {
+  // the right side is too long => rotate from the right (_not_ rightwards)
+  var nodeBefore = this.node;
+  var elementsBefore = this.elements;
+  var leftBefore = this.left;
+  this.node = this.right.node;
+  this.elements = this.right.elements;
+  this.left = this.right;
+  this.right = this.right.right;
+  this.left.right = this.left.left;
+  this.left.left = leftBefore;
+  this.left.node = nodeBefore;
+  this.left.elements = elementsBefore;
+  this.left.updateInNewLocation();
+  this.updateInNewLocation();
+};
+
+AVLTree.prototype.updateInNewLocation = function() {
+  this.getDepthFromChildren();
+};
+
+AVLTree.prototype.getDepthFromChildren = function() {
+  this.depth = this.node == null ? 0 : 1;
+  if (this.left != null) {
+    this.depth = this.left.depth + 1;
+  }
+  if (this.right != null && this.depth <= this.right.depth) {
+    this.depth = this.right.depth + 1;
+  }
+};
+
+AVLTree.prototype.compare = function(n1, n2) {
+  v1 = n1[this.attr];
+  v2 = n2[this.attr];
+  if (v1 == v2) {
+    return 0;
+  }
+  if (v1 < v2) {
+    return -1;
+  }
+  return 1;
+};
+
+AVLTree.prototype.add = function(n) {
+  var o = this.compare(n, this.node);
+  if (o == 0) {
+    this.elements.push(n);
+    return false;
+  }
+
+  var ret = false;
+  if (o == -1) {
+    if (this.left == null) {
+      this.left = new AVLTree(n, this.attr);
+      ret = true;
+    } else {
+      ret = this.left.add(n);
+      if (ret) {
+        this.balance();
+      }
+    }
+  } else if (o == 1) {
+    if (this.right == null) {
+      this.right = new AVLTree(n, this.attr);
+      ret = true;
+    } else {
+      ret = this.right.add(n);
+      if (ret) {
+        this.balance();
+      }
+    }
+  }
+
+  if (ret) {
+    this.getDepthFromChildren();
+  }
+  return ret;
+};
+
+AVLTree.prototype.findBest = function(value) {
+  var substr = this.node[this.attr].substr(0, value.length).toLowerCase();
+  var value = value.toLowerCase();
+
+  if (value < substr) {
+    if (this.left != null) {
+      return this.left.findBest(value);
+    }
+    return [];
+  }
+  else if (value > substr) {
+    if (this.right != null) {
+      return this.right.findBest(value);
+    }
+    return [];
+  }
+  return this.elements;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
