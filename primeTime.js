a35163c1c41340be29d3949336b2ec9bb787c9e2
Connor Kojimoto <cokojimoto@gmail.com>
2015-04-24 20:34:25 -0700

    adds primeTime exercise

---
+++ primeTime.js
@@ -0,0 +1,26 @@
+// Write a method that takes a maximum bound and returns all primes starting 
+// with 0 up-to and including the maximum bound.
+
+var prime = function(num) {
+  // Generate an array containing every prime number between 0 and the num specified (inclusive)
+  // create function that check for primes
+  // iterate throught from 2 to num, calling function with each iteration
+  // return answer as array
+  var ans = [];  
+  var isPrime = function(n) {
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  for (var j = 2; j <= num; j++) {
+    if (isPrime(j)) {
+      ans.push(j);
+    }
+  }
+  
+  return ans;
+};
